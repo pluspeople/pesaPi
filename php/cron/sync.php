6d32e62ed7c1ad0901ade677bd48bd8da2731cc5
Michael Pedersen <kaal@pluspeople.dk>
2014-08-21 01:05:33 +0300

    improved/fixed the include path in sync.php

--- php/cron/sync.php
+++ php/cron/sync.php
@@ -27,7 +27,7 @@
 		SUCH DAMAGE.
  */
 
-set_include_path("../../local_include:../../include:" . get_include_path());
+set_include_path("../include:" . get_include_path());
 require_once("PLUSPEOPLE/autoload.php");
 
 // Simple syncronisation example
