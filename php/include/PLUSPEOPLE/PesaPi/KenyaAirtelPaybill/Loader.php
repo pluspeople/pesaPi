6401dda240501d3474daa72d62491c075d5356d7
Michael Pedersen <kaal@pluspeople.dk>
2014-11-10 00:03:39 +0100

    Airtel Paybill improvements
    I have been working on the Airtel Paybill support.
    The main http requests flow is now sorted - Airtel Paybill is now almost working.

--- php/include/PLUSPEOPLE/PesaPi/KenyaAirtelPaybill/Loader.php
+++ php/include/PLUSPEOPLE/PesaPi/KenyaAirtelPaybill/Loader.php
@@ -50,7 +50,12 @@ class Loader {
 		curl_setopt($this->curl, CURLOPT_COOKIEJAR, $this->cookieFile);
 		curl_setopt($this->curl, CURLOPT_HEADER, true);
 		curl_setopt($this->curl, CURLOPT_FOLLOWLOCATION, true);
-		curl_setopt($this->curl, CURLOPT_SSL_VERIFYHOST, 0);
+		curl_setopt($this->curl, CURLOPT_SSL_VERIFYHOST, false);
+		curl_setopt($this->curl, CURLOPT_SSL_VERIFYPEER, false);
+		curl_setopt($this->curl, CURLOPT_USERAGENT, "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv:33.0) Gecko/20100101 Firefox/33.0");
+		curl_setopt($this->curl, CURLOPT_HTTPHEADER, array('Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8', 
+																											 'Accept-Language: en-US,en;q=0.5'));
+
 	}
 
 	public function retrieveData($fromTime) {
@@ -62,8 +67,8 @@ class Loader {
 				// change password
 				$this->changePassword($login);
 			}
-
-			$pages = $this->loadResults($search, $fromTime);
+			$searchForm = $this->loadSearchForm($login);
+ 			$pages = $this->loadResults($searchForm, $fromTime);
 		}
 		// return the reverse array - we want the oldest data first.
 		return array_reverse($pages);
@@ -76,9 +81,9 @@ class Loader {
 		curl_setopt($this->curl, CURLOPT_URL, $this->baseUrl . "/Login.aspx");
 		curl_setopt($this->curl, CURLOPT_POST, false);
 		$first = curl_exec($this->curl);
+ 		file_put_contents("STEP0_LOGIN.txt", $first);
 
-
-		$viewState = $this->getViewState($first);
+		$viewState = $this->getHidden($first);
 		$postData = 
 			'__EVENTTARGET=' . 
 			'&__EVENTARGUMENT=' . 
@@ -98,89 +103,86 @@ class Loader {
 		curl_setopt($this->curl, CURLOPT_COOKIEFILE, $this->cookieFile); 
 
 		$login = curl_exec($this->curl);
- 		file_put_contents("STEP0_LOGIN.txt", $login);
+ 		file_put_contents("STEP1_POST_LOGIN.txt", $login);
 		// TODO: missing error detection
 		return $login;
 	}
 
+	private function loadSearchForm($searchPage) {
+		$viewState = $this->getHidden($searchPage);
+		$prevPage = $this->getHidden($searchPage, '__PREVIOUSPAGE');
+		$eventValidation = $this->getHidden($searchPage, '__EVENTVALIDATION');
 
+		$postData = 
+			'__EVENTTARGET=ctl00%24LnkReports' .
+			'&__EVENTARGUMENT=' .
+			'&__VIEWSTATE=' . urlencode($viewState) .
+			'&__PREVIOUSPAGE=' . urlencode($prevPage) . 
+			'&__EVENTVALIDATION=' . urlencode($eventValidation);
+
+		curl_setopt($this->curl, CURLOPT_URL, $this->baseUrl . "/NewTransctionsReport.aspx");
+		curl_setopt($this->curl, CURLOPT_POST, true);
+		curl_setopt($this->curl, CURLOPT_POSTFIELDS, $postData); 
+		curl_setopt($this->curl, CURLOPT_COOKIEFILE, $this->cookieFile); 
+		curl_setopt($this->curl, CURLOPT_REFERER, 'https://41.223.56.58:7556/NewZapMerchant.aspx');
+
+		// TODO: needs to retrieve the following pages, in case there is more than 500 entries
+		$searchForm = curl_exec($this->curl);
+		file_put_contents("STEP2_STATEMENT.txt", $searchForm);
+
+		return $searchForm;
+	}
 
 	private function loadResults($searchPage, $fromTime) {
-		return "";
-		$fromTime = (int)$fromTime;
 		$pages = array();
-		if ($fromTime > 0) {
-			$viewState = $this->getViewState($searchPage);
-			$accounts = $this->findAccounts($searchPage);
-			$account = $accounts[2];
-			$now = time();
+		$viewState = $this->getHidden($searchPage);
+		$prevPage = $this->getHidden($searchPage, '__PREVIOUSPAGE');
+		$eventValidation = $this->getHidden($searchPage, '__EVENTVALIDATION');
 
-			$postData = 
-				'__VIEWSTATE=' . urlencode($viewState) .
-				'&ctl00$Main$ctl00$ctlDatePicker$dlpagesize_Input=' . '500' .
-				'&ctl00$Main$ctl00$ctlDatePicker$dlpagesize_text=' . '500' .
-				'&ctl00$Main$ctl00$ctlDatePicker$dlpagesize_value=' . '500' .
-				'&ctl00$Main$ctl00$ctlDatePicker$dlpagesize_index=' . '3' .
-				'&ctl00_Main_ctl00_ctlDatePicker_datePickerStartDate=' . date("Y-m-d", $fromTime) . 
-				'&ctl00$Main$ctl00$ctlDatePicker$datePickerStartDate$dateInput=' . urlencode(date("Y-m-d H:i:s", $fromTime)) .
-				'&ctl00$Main$ctl00$ctlDatePicker$datePickerStartDate$dateInput_TextBox=' . date("Y-m-d", $fromTime) .
-				'&ctl00_Main_ctl00_ctlDatePicker_datePickerStartDate_calendar_SD=' . urlencode('[]') .
-				'&ctl00_Main_ctl00_ctlDatePicker_datePickerEndDate=' . date("Y-m-d", $now) . 
-				'&ctl00$Main$ctl00$ctlDatePicker$datePickerEndDate$dateInput=' . urlencode(date("Y-m-d H:i:s", $now)) .
-				'&ctl00$Main$ctl00$ctlDatePicker$datePickerEndDate$dateInput_TextBox=' . date("Y-m-d", $now) .
-				'&ctl00_Main_ctl00_ctlDatePicker_datePickerEndDate_calendar_SD=' . urlencode('[]') .
-				'&ctl00$Main$ctl00$cbAccountType_Input=' . urlencode($account[1]) .
-				'&ctl00$Main$ctl00$cbAccountType_text=' . urlencode($account[1]) .
-				'&ctl00$Main$ctl00$cbAccountType_value=' . urlencode($account[2]) . 
-				'&ctl00$Main$ctl00$cbAccountType_index=' . $account[0] . 
-				'&ctl00$Main$ctl00$rblTransType=' . 'All' .
-				'&ctl00$Main$ctl00$btnSearch=' . 'Search' .
-				'&ctl00$Main$ctl00$cpeExpandedFilter_ClientState=' . '' . // unkown 
-				'&ctl00_Main_ctl00_AccountStatementGrid1_dgStatementPostDataValue=' . '' // unkown
-				;
-			
-			curl_setopt($this->curl, CURLOPT_URL, $this->baseUrl . "/ke/Main/home2.aspx?MenuID=1826");
-			curl_setopt($this->curl, CURLOPT_POST, true);
-			curl_setopt($this->curl, CURLOPT_POSTFIELDS, $postData); 
-			curl_setopt($this->curl, CURLOPT_COOKIEFILE, $this->cookieFile); 
+		$postData = 
+			'__EVENTTARGET=' .
+			'&__EVENTARGUMENT=' .
+			'&__LASTFOCUS=' .
+			'&__VIEWSTATE=' . urlencode($viewState) .
+			'&__PREVIOUSPAGE=' . urlencode($prevPage) . 
+			'&__EVENTVALIDATION=' . urlencode($eventValidation) .
+			'&ctl00%24ContentPlaceHolder1%24rdReportType=My+Reports' .
+			'&ctl00%24ContentPlaceHolder1%24rdbTrans=FUNDS'.
+			'&ctl00_ContentPlaceHolder1_dtFromDate_Raw=1414713600000' . // not done
+			'&ctl00%24ContentPlaceHolder1%24dtFromDate=31-Oct-2014' . // not done
+			'&ctl00_ContentPlaceHolder1_dtFromDate_DDDWS=0%3A0%3A12000%3A787%3A270%3A0%3A-10000%3A-10000' . // not done
+			'&ctl00_ContentPlaceHolder1_dtFromDate_DDD_C_FNPWS=0%3A0%3A-1%3A-10000%3A-10000%3A0%3A0px%3A-10000' . // not done
+			'&ctl00%24ContentPlaceHolder1%24dtFromDate%24DDD%24C=11%2F01%2F2014%3A10%2F31%2F2014' . // not done
+			'&ctl00_ContentPlaceHolder1_dtTODate_Raw=1415491200000' . // not done
+			'&ctl00%24ContentPlaceHolder1%24dtTODate=09-Nov-2014' . // not done
+			'&ctl00_ContentPlaceHolder1_dtTODate_DDDWS=0%3A0%3A12000%3A1005%3A270%3A0%3A-10000%3A-10000' . // not done
+			'&ctl00_ContentPlaceHolder1_dtTODate_DDD_C_FNPWS=0%3A0%3A-1%3A-10000%3A-10000%3A0%3A0px%3A-10000' . // not done
+			'&ctl00%24ContentPlaceHolder1%24dtTODate%24DDD%24C=11%2F09%2F2014%3A11%2F09%2F2014' . // not done
+			'&ctl00%24ContentPlaceHolder1%24btnViewAudit.x=' . (String)rand(1,79) .
+			'&ctl00%24ContentPlaceHolder1%24btnViewAudit.y=' . (String)rand(1,24) .
+			'&DXScript=1_23%2C2_21%2C2_28%2C2_20%2C1_27%2C1_44%2C1_41%2C2_15'; // not done
 
-			// TODO: needs to retrieve the following pages, in case there is more than 500 entries
-			$result = curl_exec($this->curl);
-			//  		file_put_contents("STEP1_STATEMENT.txt", $result);
-			// TODO: missing error detection
-			$pages[] = $result;
-		}
+		curl_setopt($this->curl, CURLOPT_URL, $this->baseUrl . "/NewTransctionsReport.aspx");
+		curl_setopt($this->curl, CURLOPT_POST, true);
+		curl_setopt($this->curl, CURLOPT_POSTFIELDS, $postData); 
+		curl_setopt($this->curl, CURLOPT_COOKIEFILE, $this->cookieFile); 
+		curl_setopt($this->curl, CURLOPT_REFERER, 'https://41.223.56.58:7556/NewTransctionsReport.aspx');
+
+		// TODO: needs to retrieve the following pages, in case there is more than 500 entries
+		$pages[] = curl_exec($this->curl);
+		file_put_contents("STEP3_RESULT.txt", $pages[0]);
+
 		return $pages;
 	}
 
-	private function getViewState($input) {
+	private function getHidden($input, $tag="__VIEWSTATE") {
 		$temp = array();
-		preg_match("/(?<=__VIEWSTATE\" value=\")(?<val>.*?)(?=\")/", $input, $temp);
+		preg_match("/(?<=" . $tag . "\" value=\")(?<val>.*?)(?=\")/", $input, $temp);
 		return isset($temp[1]) ? $temp[1] : "";
 	}
 
-	/*
-		Finds all the accounts available.
-		Returns a 2-dimensional array with the following format:
-		[[offset, name, account-no]]
-	 */
-	private function findAccounts($input) {
-		$results = array();
-		$temp = array();
-		preg_match('/ctl00\$Main\$ctl00\$cbAccountType.+ScrollDownDisabled\.gif"\},(\[.+])\);<\/script>/U', $input, $temp);
-
-		if (isset($temp[1])) {
-			$accounts = json_decode($temp[1]);
-			$count = 0;
-			foreach ($accounts AS $account) {
-				$results[] = array($count++, $account->Text, $account->Value);
-			}
-		}
-		return $results;
-	}
-
 	private function changePassword($page) {
-		$viewState = $this->getViewState($page);
+		$viewState = $this->getHidden($page);
 		$oldPassword = $this->getPassword();
 		
 		$toEmail = $this->config->getConfig("AdminEmail");
